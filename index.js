8ff5d62da94c0f7340cb636c520143fb1943d613
swetlina satpathy <swetlinasatpathy@swetlinas-MacBook-Pro.local>
2024-05-31 14:33:54 +0530

    arbitrage api created

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { StringDecoder } = require("string_decoder");
+//const fetch = require("node-fetch"); // Import the fetch library
+const zlib = require('zlib');
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/arbitrage", (request, response) => {
+    const url = "https://www.moneycontrol.com/stocks/fno/marketstats/arbitrage/futures-spot-near-2.html";
+    let data = {};
+    const options = {
+        headers: {
+            "Content-Type": "text/html",
+        },
+    };
+
+    async function fetchStream() {
+        let res = await fetch(url)
+        console.log("streaming started--")
+        const streamReader = res.body.pipeThrough(new TextDecoderStream()).getReader();
+
+        while(true){
+            const { done, value} = await streamReader.read();
+            if(done){
+                break;
+            }
+            console.log("chunk received--", value)
+            response.write(value)
+        }
+        console.log("streaming complete")
+        response.end()
+    }
+
+    fetchStream()
+});
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
