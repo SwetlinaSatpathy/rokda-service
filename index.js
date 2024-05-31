8e03932261b198421252e3faeadf76b0e072c568
swetlina satpathy <swetlinasatpathy@swetlinas-MacBook-Pro.local>
2024-05-31 18:00:54 +0530

    Dynamic URLs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { StringDecoder } = require("string_decoder");
+const {TextDecoderStream} = required ("web-streams-polyfill");
+//const { StringDecoder } = require("string_decoder");
 //const fetch = require("node-fetch"); // Import the fetch library
 const zlib = require('zlib');
 const app = express();
@@ -10,31 +11,42 @@ const PORT = process.env.PORT || 4000;
 app.get("/arbitrage", (request, response) => {
     //const url = "https://www.moneycontrol.com/stocks/fno/marketstats/arbitrage/futures-spot-next-1.html";
     //const url = "https://www.rbi.org.in/Scripts/BS_NSDPDisplay.aspx?param=4#";
-    const url = "https://www.google.co.in/search?q=tata%20Motors%20share%20price";
+    //const url = "https://www.google.co.in/search?q=tata%20Motors%20share%20price";
+    const url     = request.url.slice(1);
+    const options    = request.body || {};
+    //const options = request.options || {};
+
+
+    console.log(`Received URL: ${url}`);
+    console.log(`Received Options: ${JSON.stringify(options)}`);
     let data = {};
-    const options = {
-        headers: {
-            "Content-Type": "text/html",
-        },
-    };
+    // const options = {
+    //     headers: {
+    //         "Content-Type": "text/html",
+    //     },
+    // };
 
     async function fetchStream() {
-        let res = await fetch(url)
-        console.log("streaming started--")
-        const streamReader = res.body.pipeThrough(new TextDecoderStream()).getReader();
+        try {
+            let res = await fetch(url, options)
+            console.log("streaming started--")
+            const streamReader = res.body.pipeThrough(new TextDecoderStream()).getReader();
 
-        while(true){
-            const { done, value} = await streamReader.read();
-            if(done){
-                break;
+            while(true){
+                const {done, value} = await streamReader.read();
+                if(done){
+                    break;
+                }
+                console.log("chunk received--", value)
+                response.write(value)
             }
-            console.log("chunk received--", value)
-            response.write(value)
+            console.log("streaming complete")
+            response.end()
+        }catch (error){
+            console.error(`Error occured: ${error}`);
+            response.status(500).send(`Error fetching the URL: ${url}`)
         }
-        console.log("streaming complete")
-        response.end()
     }
-
     fetchStream()
 });
 
